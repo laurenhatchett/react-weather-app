b4822e675a8ab26db50ff1a77b322f1bf4a07770
laurenhatchett <laurenhatchett@gmail.com>
2023-01-15 10:39:15 -0500

    add searchLocation function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import axios from 'axios'
 
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
   
-  //const url =`https://api.openweathermap.org/data/2.5/weather?q=charlotte&appid=48425367274a044c1e0f5043a3c6b8c2`
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=48425367274a044c1e0f5043a3c6b8c2`
   
+  const searchLocation = (event) => {
+    if (event.key === 'Enter'){
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+  }
+
   return (
     <div className='app'>
+      <div className='search'>
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type = "text" />
+      </div>
       <div className='container'>
         <div className='top'>
           <div className='location'>
